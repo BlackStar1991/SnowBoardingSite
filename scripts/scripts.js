6789130ea44c34afda96ac1979681c67f5786869
BlackStar1991 <zirkaandry@ukr.net>
2017-03-04 16:06:23 +0200

    3 commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -120,19 +120,38 @@ window.onload = function () {
 
 
 
-    $("[data-fancybox]").fancybox({
-        beforeShow: function () {
-            $("body *:not(.fancybox-overlay, .fancybox-overlay *)").addClass("blur");
-        },
-        afterClose: function () {
-            $("body *:not(.fancybox-overlay, .fancybox-overlay *)").removeClass("blur");
-        },
-        helpers : {
-            overlay: {
-                opacity: 0.8,
-                css: {'background-color': '#ff0000'}
-            }
-        }
+    // $("[data-fancybox]").fancybox({
+    //     beforeShow: function () {
+    //         $("body *:not(.fancybox-overlay, .fancybox-overlay *)").addClass("blur");
+    //     },
+    //     afterClose: function () {
+    //         $("body *:not(.fancybox-overlay, .fancybox-overlay *)").removeClass("blur");
+    //     },
+    //     helpers : {
+    //         overlay: {
+    //             opacity: 0.8,
+    //             css: {'background-color': '#ff0000'}
+    //         }
+    //     }
+    // });
+
+    $("a.fancyImg").fancybox({
+        "controls":true,
+        "padding" : 20,
+        "imageScale" : false,
+        "zoomOpacity" : false,
+        "zoomSpeedIn" : 1000,
+        "zoomSpeedOut" : 1000,
+        "zoomSpeedChange" : 1000,
+        "frameWidth" : 700,
+        "frameHeight" : 600,
+        "overlayShow" : true,
+        "overlayOpacity" : 0.8,
+        "hideOnContentClick" :false,
+        "centerOnScroll" : false,
+        'speedIn': 600,
+        'speedOut': 200
+
     });
 
 
