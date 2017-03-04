6d0569901484084992288d832a9ca7950e52c02b
BlackStar1991 <zirkaandry@ukr.net>
2017-03-04 14:04:03 +0200

    second commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -121,25 +121,19 @@ window.onload = function () {
 
 
     $("[data-fancybox]").fancybox({
-
-
-            // "padding" : 20,
-            // "imageScale" : false,
-            // "zoomOpacity" : false,
-            // "zoomSpeedIn" : 1000,
-            // "zoomSpeedOut" : 1000,
-            // "zoomSpeedChange" : 1000,
-            // "frameWidth" : 700,
-            // "frameHeight" : 600,
-            // "overlayShow" : true,
-            // "overlayOpacity" : 0.8,
-            // "hideOnContentClick" :false,
-            // "centerOnScroll" : false,
-            // 'speedIn': 600,
-            // 'speedOut': 200
-
-        });
-
+        beforeShow: function () {
+            $("body *:not(.fancybox-overlay, .fancybox-overlay *)").addClass("blur");
+        },
+        afterClose: function () {
+            $("body *:not(.fancybox-overlay, .fancybox-overlay *)").removeClass("blur");
+        },
+        helpers : {
+            overlay: {
+                opacity: 0.8,
+                css: {'background-color': '#ff0000'}
+            }
+        }
+    });
 
 
 };
